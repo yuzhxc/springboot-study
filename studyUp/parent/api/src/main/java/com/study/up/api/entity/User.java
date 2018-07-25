4b0fdf706e4ab71fbd62ba2237583071368b2bb3
yuzhaoxin <979735777@qq.com>
2018-07-26 00:12:27 +0800

    删掉

--- studyUp/parent/api/src/main/java/com/study/up/api/entity/User.java
+++
@@ -1,9 +0,0 @@
-package com.study.up.api.entity;
-
-import lombok.Data;
-
-@Data
-public class User {
-    private String name;
-    private String code;
-}
--- studyUp/parent/api/src/main/java/com/study/up/api/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.study.up.api.service;
-
-import com.study.up.api.entity.User;
-
-public interface UserService {
-    User queryByPk(String pk);
-}
